f6789ca8f13e7d84d36cbfed1fc76fc591d97f1f
Akash Dhiman <77789071+Akash-Dhiman@users.noreply.github.com>
2021-01-23 19:55:52 +0530

    Update index.js

--- Day2Assignment/index.js
+++ Day2Assignment/index.js
@@ -1,11 +1,11 @@
 console.log(1 + "2" + "2");// output:122 because it concatenate the string format.
 
 
-console.log(1 + +"2" + "2");//output:32 because it has 1 + + "2" it add or perform addition operation.
+console.log(1 + +"2" + "2");//output:32 because it has 1 + + "2" it add or perform concatenate operation.
 
-console.log(1 + -"1" + "2");//output: 02 because it perform minus opertion 
+console.log(1 + -"1" + "2");//output: 02 it perform minus opertion because operators are simultaneously occured.
 
-console.log(+"1" + "1" + "2");// output:112 because it perform concatenation of a string.
+console.log(+"1" + "1" + "2");// output:112 because it perform concatenation of a string and not affected by +"1".
 
 console.log( "A" - "B" + "2");// output:NaN2 because string a can't be perform delete operation and it add "2" as concatenate string.
 
