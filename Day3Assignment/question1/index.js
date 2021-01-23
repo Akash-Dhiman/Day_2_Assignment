d3ba5b0c8e5c5ebad28fef62ec88096b2487c4c0
Akash Dhiman <77789071+Akash-Dhiman@users.noreply.github.com>
2021-01-23 13:38:58 +0530

    Add files via upload

---
+++ Day3Assignment/question1/index.js
@@ -0,0 +1,15 @@
+// What is the value of clothes[0] and why?
+// const clothes = ['jacket', 't-shirt'];
+// clothes.length = 0;
+// console.log(clothes[0]);
+
+
+const clothes = ['jacket', 't-shirt'];
+
+clothes.length = 0;
+
+console.log(clothes[0]);
+
+
+
+//The Output of this program is undefined because we can set the lenght of the clothes not an array that's why the output is undefined
